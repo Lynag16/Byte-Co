dc14acea9cc0ad7330efe4323075f3563dfeeda6
Agouazi Lynda <lynda.agouazi.etu@b13p23.fil.univ-lille.fr>
2024-12-09 15:29:59 +0100

    add cors and security config

--- microservices/authentification/src/main/java/com/glop/authentification/security/SecurityConfig.java
+++ microservices/authentification/src/main/java/com/glop/authentification/security/SecurityConfig.java
@@ -1,38 +1,52 @@
-//package com.glop.authentification.security;
-//
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-//import org.springframework.security.crypto.password.PasswordEncoder;
-//import org.springframework.security.web.SecurityFilterChain;
-//
-//@Configuration
-//@EnableWebSecurity
-//public class SecurityConfig {
-//
-//    @Bean
-//    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-//        http
-//            .csrf().disable() 
-//            .authorizeHttpRequests(auth -> auth
-//            		.requestMatchers("/h2-console/**").permitAll()
-//            		.requestMatchers("/api/clients/register", "/api/clients/login").permitAll()
-//                .anyRequest().authenticated()
-//            )
-//            .formLogin()
-//                .permitAll()
-//            .and()
-//            .logout()
-//                .permitAll();
-//
-//        return http.build();
-//    }
-//
-//    @Bean
-//    public PasswordEncoder passwordEncoder() {
-//        return new BCryptPasswordEncoder();
-//    }
-//
-//}
+package com.glop.authentification.security;
+
+import java.util.Arrays;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+            .csrf(AbstractHttpConfigurer::disable) 
+            .cors() // Active la configuration CORS
+            .and()
+            .authorizeHttpRequests(auth -> auth
+                .requestMatchers("/api/clients/register", "/api/clients/login","/api/clients/reset-password", "/api/partenaires/register", "/api/partenaires/login", "/api/partenaires/reset-password","/api/personnels/register", "/api/personnels/login","/api/personnels/reset-password") 
+                .permitAll() 
+                .anyRequest() 
+                .authenticated()
+            )
+            .formLogin(withDefaults()) // Utilise le formulaire de login standard
+            .logout(withDefaults()); // Utilise la déconnexion standard
+        return http.build();
+    }
+
+ 
+   // Configuration CORS
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000")); 
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD"));
+        configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
+        configuration.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+
+}
+
