49d67814b3ce335f3ed3a54a0ddaf374265ceb7f
laraibi.mohammadjavad.etu <laraibi.mohammadjavad.etu@univ-lille.fr>
2025-01-06 13:32:34 +0100

    gestion des offre et contrats et la relation entre les deux marche bien

--- microservices/gestioncontrats/src/main/java/glop/gestioncontrats/GestioncontratsApplication.java
+++ microservices/gestioncontrats/src/main/java/glop/gestioncontrats/GestioncontratsApplication.java
@@ -5,6 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class GestioncontratsApplication {
+	public GestioncontratsApplication() {
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(GestioncontratsApplication.class, args);
