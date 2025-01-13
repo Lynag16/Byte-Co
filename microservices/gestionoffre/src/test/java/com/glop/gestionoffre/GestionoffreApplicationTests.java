c37bfeb059be431ce4878b13a54fca0213415f4c
Agouazi Lynda <lynda.agouazi.etu@b13p23.fil.univ-lille.fr>
2025-01-13 11:53:07 +0100

    initialisation of the offers microservice

---
+++ microservices/gestionoffre/src/test/java/com/glop/gestionoffre/GestionoffreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.glop.gestionoffre;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionoffreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
