dfae23ab581e91ac92af1498426907945277bbe7
RhofirAbdelali <abdo.rhofir@gmail.com>
2025-02-23 14:46:29 +0100

    corriger branch origin/front

--- microservices/gestioncontrats/src/main/java/glop/gestioncontrats/repositories/OfferRepository.java
+++
@@ -1,9 +0,0 @@
-package glop.gestioncontrats.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import glop.gestioncontrats.entities.Offer;
-
-public interface OfferRepository extends JpaRepository<Offer, Integer> {
-
-}
