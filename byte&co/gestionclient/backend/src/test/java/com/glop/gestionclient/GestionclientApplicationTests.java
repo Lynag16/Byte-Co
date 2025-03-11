85b2148ae0574525d6338e1ff29acb8bfda20093
Agouazi Lynda <lynda.agouazi.etu@b13p23.fil.univ-lille.fr>
2025-03-11 09:39:47 +0100

    dockernirisation of gestionclient microservice

---
+++ byte&co/gestionclient/backend/src/test/java/com/glop/gestionclient/GestionclientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.glop.gestionclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionclientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
