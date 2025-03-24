9ad6fe8a91f48822848feb5e1c0b5f917d8ccccf
Agouazi Lynda <lynda.agouazi.etu@b13p23.fil.univ-lille.fr>
2025-03-24 12:16:55 +0100

    adding the empreintecarpone microservice

---
+++ byte&co/empreintecarbone/src/main/java/com/glop/empreintecarbone/EmpreintecarboneApplication.java
@@ -0,0 +1,13 @@
+package com.glop.empreintecarbone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmpreintecarboneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmpreintecarboneApplication.class, args);
+	}
+
+}
---
+++ byte&co/empreintecarbone/src/test/java/com/glop/empreintecarbone/EmpreintecarboneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.glop.empreintecarbone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmpreintecarboneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
