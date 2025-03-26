9d962b15b7f82d5737e13a0db53f3f533ebb547e
Totoum Mandoum Patricia <patricia.totoummandoum.etu@a10p28.fil.univ-lille.fr>
2025-03-26 18:46:08 +0100

    repositories avec usage standard et filtre

--- byte&co/gestionsinistres/src/main/java/com/glop/gestionsinistres/repository/DetailsSinistreRepository.java
+++ byte&co/gestionsinistres/src/main/java/com/glop/gestionsinistres/repository/DetailsSinistreRepository.java
@@ -2,5 +2,10 @@ package com.glop.gestionsinistres.repository;
 
 import com.glop.gestionsinistres.model.DetailsSinistre;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public interface DetailsSinistreRepository extends JpaRepository<DetailsSinistre, Long> {}
+@Repository
+public interface DetailsSinistreRepository extends JpaRepository<DetailsSinistre, Long> {
+
+    DetailsSinistre findBySinistreId(Long sinistreId);
+}
