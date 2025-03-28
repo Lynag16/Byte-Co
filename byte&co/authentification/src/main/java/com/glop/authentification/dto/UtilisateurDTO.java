6652732f7a7b183d5a4a544163397c47a553a9ce
patriciaTot <patricia.totoummandoum.etu@univ-lille.fr>
2025-03-28 03:59:37 +0100

    recuperation des infos de l utilisateur pour la creation de sinistre OK back et front

--- byte&co/authentification/src/main/java/com/glop/authentification/dto/UtilisateurDTO.java
+++ byte&co/authentification/src/main/java/com/glop/authentification/dto/UtilisateurDTO.java
@@ -1,20 +1,37 @@
 package com.glop.authentification.dto;
 
 public class UtilisateurDTO {
+    private Long idUtilisateur;
     private String email;
     private String motDePasse;
     private String typeUtilisateur;
+    private String nom;
+    private String prenom;
+    private String dateNaissance;
+    private String codePostal;
 
     // Constructeurs
     public UtilisateurDTO() {}
 
-    public UtilisateurDTO(String email, String motDePasse, String typeUtilisateur) {
+    public UtilisateurDTO(String email, String motDePasse, String typeUtilisateur, String nom, String prenom, String dateNaissance, String codePostal) {
         this.email = email;
         this.motDePasse = motDePasse;
         this.typeUtilisateur = typeUtilisateur;
+        this.nom = nom;
+        this.prenom = prenom;
+        this.dateNaissance = dateNaissance;
+        this.codePostal = codePostal;
     }
 
     // Getters et Setters
+    public Long getId() {
+        return idUtilisateur;
+    }
+
+    public void setId(Long idUtilisateur) {
+        this.idUtilisateur = idUtilisateur;
+    }
+    
     public String getEmail() {
         return email;
     }
@@ -37,5 +54,37 @@ public class UtilisateurDTO {
 
     public void setTypeUtilisateur(String typeUtilisateur) {
         this.typeUtilisateur = typeUtilisateur;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    public String getDateNaissance() {
+        return dateNaissance;
+    }
+
+    public void setDateNaissance(String dateNaissance) {
+        this.dateNaissance = dateNaissance;
+    }
+
+    public String getCodePostal() {
+        return codePostal;
+    }
+
+    public void setCodePostal(String codePostal) {
+        this.codePostal = codePostal;
     }
 }
--- byte&co/authentification/src/main/java/com/glop/authentification/entities/Utilisateur.java
+++ byte&co/authentification/src/main/java/com/glop/authentification/entities/Utilisateur.java
@@ -10,6 +10,7 @@ import jakarta.persistence.Table;
 @Entity
 @Table(name = "Utilisateur")
 public class Utilisateur {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -21,7 +22,24 @@ public class Utilisateur {
     private String motDePasse;
 
     @Column(nullable = false)
-    private String typeUtilisateur = "USER"; // Default value
+    private String typeUtilisateur = "USER";
+
+    private String nom;
+    private String prenom;
+    private String dateNaissance;
+    private String codePostal;
+
+    public Utilisateur() {}
+
+    public Utilisateur(String email, String motDePasse, String typeUtilisateur, String nom, String prenom, String dateNaissance, String codePostal) {
+        this.email = email;
+        this.motDePasse = motDePasse;
+        this.typeUtilisateur = typeUtilisateur;
+        this.nom = nom;
+        this.prenom = prenom;
+        this.dateNaissance = dateNaissance;
+        this.codePostal = codePostal;
+    }
 
     public Long getId() { return id; }
     public void setId(Long id) { this.id = id; }
@@ -34,5 +52,18 @@ public class Utilisateur {
 
     public String getTypeUtilisateur() { return typeUtilisateur; }
     public void setTypeUtilisateur(String typeUtilisateur) { this.typeUtilisateur = typeUtilisateur; }
+
+    public String getNom() { return nom; }
+    public void setNom(String nom) { this.nom = nom; }
+
+    public String getPrenom() { return prenom; }
+    public void setPrenom(String prenom) { this.prenom = prenom; }
+
+    public String getDateNaissance() { return dateNaissance; }
+    public void setDateNaissance(String dateNaissance) { this.dateNaissance = dateNaissance; }
+
+    public String getCodePostal() { return codePostal; }
+    public void setCodePostal(String codePostal) { this.codePostal = codePostal; }
+
 }
 
